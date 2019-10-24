7949558067f9d7985b6d899a6d2d9de12ccea089
Evillivekai <kaikai1221an@hotmail.com>
2019-10-23 22:03:06 -0700

    For loop logical wrong

--- db/seed.js
+++ db/seed.js
@@ -2,6 +2,6 @@ const db = require('.');
 // eslint-disable-next-line camelcase
 const dummy_data = require('./dummy-data.json');
 
-for (let i = 0; i < 10; i + 1) {
+for (let i = 0; i < 10; i += 1) {
   db.saveCReviews(dummy_data);
 }
